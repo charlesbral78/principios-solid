c345a00c9e42727e832b013927e7702a9637a559
â[fcharles <charlesbral78@hotmail.com>
2023-09-07 15:15:42 -0300

    Projeto utilizando os cinco conceitos de SOLID

---
+++ src/br/com/alura/rh/service/tributacao/Anuenio.java
@@ -0,0 +1,23 @@
+package br.com.alura.rh.service.tributacao;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Anuenio implements Reajuste {
+
+    private BigDecimal valor;
+    private LocalDate data;
+
+    public Anuenio(BigDecimal valor) {
+        this.valor = valor;
+        this.data = LocalDate.now();
+    }
+
+    public BigDecimal valor() {
+        return valor;
+    }
+
+    public LocalDate data() {
+        return data;
+    }
+}
---
+++ src/br/com/alura/rh/service/tributacao/Promocao.java
@@ -0,0 +1,28 @@
+package br.com.alura.rh.service.tributacao;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Promocao implements ReajusteTributavel{
+
+    private BigDecimal valor;
+    private LocalDate data;
+
+    public Promocao(BigDecimal valor) {
+        this.valor = valor;
+        this.data = LocalDate.now();
+    }
+
+    public BigDecimal valor() {
+        return valor;
+    }
+
+    public LocalDate data() {
+        return data;
+    }
+
+    public BigDecimal valorImpostoDeRenda() {
+        return valor.multiply(new BigDecimal("0.1"));
+    }
+
+}
